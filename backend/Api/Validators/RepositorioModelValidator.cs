6fa606455c9798f0d00fdf21ef157c160b0ae649
Lucas Soares <soares0lucas8@gmail.com>
2026-04-05 11:25:42 -0300

    refactor: atualizando validacoes ao adicionar repositorio favorito

--- backend/Api/Validators/RepositorioModelValidator.cs
+++ backend/Api/Validators/RepositorioModelValidator.cs
@@ -16,19 +16,22 @@ public sealed class RepositorioModelValidator : AbstractValidator<RepositorioMod
             .WithMessage("A URL do repositório é obrigatória.");
 
         RuleFor(x => x.QuantidadeEstrelas)
-            .NotEmpty()
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
             .WithMessage("A quantidade de estrelas é obrigatória.")
             .GreaterThanOrEqualTo(0)
             .WithMessage("A quantidade de estrelas não pode ser negativa.");
 
         RuleFor(x => x.QuantidadeForks)
-            .NotEmpty()
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
             .WithMessage("A quantidade de forks é obrigatória.")
             .GreaterThanOrEqualTo(0)
             .WithMessage("A quantidade de forks não pode ser negativa.");
 
         RuleFor(x => x.QuantidadeObservadores)
-            .NotEmpty()
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
             .WithMessage("A quantidade de observadores é obrigatória.")
             .GreaterThanOrEqualTo(0)
             .WithMessage("A quantidade de observadores não pode ser negativa.");
